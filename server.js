1a69d395581b6cd357fa4b903ea521a9be29e545
trddy <59534885+trageddy@users.noreply.github.com>
2020-01-26 21:37:33 +0100

    Created server.js
    Added some functions.

---
+++ server.js
@@ -0,0 +1,6 @@
+function create(len) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < len; i++) text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
